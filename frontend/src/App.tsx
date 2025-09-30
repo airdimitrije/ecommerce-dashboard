29190a8e3514b6bfcba06e95c9cb39a2963e57b4
airdimitrije <airdimitrije@gmail.com>
2025-09-30 14:36:13 +0200

    Switch to HashRouter and remove 404.html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 import ProductsPage from "./pages/ProductsPage"
 import InventoryPage from "./pages/InventoryPage"
@@ -7,18 +7,13 @@ import CategoriesPage from "./pages/CategoriesPage"
 
 function App() {
   return (
-    <Router basename="/ecommerce-dashboard">
+    <Router>
       <Routes>
-        {/* Kada odeš na /ecommerce-dashboard, odmah Dashboard */}
         <Route path="/" element={<Dashboard />} />
-
-        {/* Ostale rute */}
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/inventory" element={<InventoryPage />} />
         <Route path="/orders" element={<OrdersPage />} />
         <Route path="/categories" element={<CategoriesPage />} />
-
-        {/* Ako ruta ne postoji */}
         <Route path="*" element={<h1 style={{ color: "white" }}>404 - Stranica nije pronađena</h1>} />
       </Routes>
     </Router>
